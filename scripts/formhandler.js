3a62607c850cd33db882ea34e7b6f3c9ba88f48b
seong <email>
2019-09-23 11:15:59 -0700

    first comit

---
+++ scripts/formhandler.js
@@ -0,0 +1,12 @@
+(function (window) {
+  "use strict";
+  var App = window.App || {};
+
+  function FormHandler() {
+    // Code will go here
+  }
+
+  App.FormHandler = FormHandler;
+  window.App = App;
+
+})(window);
